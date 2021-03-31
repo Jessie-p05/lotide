8938bd5dc22d139597cd79f6983c90fd6545d4cf
Jessie-p05 <jxpei05@gmail.com>
2021-03-31 23:27:08 +0000

    count the letter of a string

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+   const noSpaces = str.replace(/ /g, "");
+   const countObj = {};
+  for (let letter of noSpaces) {
+    if (countObj[letter]){
+      countObj[letter] += 1;
+    } else {
+      countObj[letter] = 1;
+    }
+  }
+  return countObj
+}
+
+console.log(countLetters("lighthouse in the house"));
