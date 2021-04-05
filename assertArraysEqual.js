f715cd1faf3598bd30287f9e74442242c4d8779b
Jessie-p05 <jxpei05@gmail.com>
2021-04-05 21:12:20 +0000

    rafactor the asserEqualqArrays.js with seperated assertion file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,4 @@
+const eqArrays = require('./eqArrays')
 const assertArraysEqual = function (array1, array2) {
   const equalResult = eqArrays(array1, array2);
   if (equalResult === true) {
@@ -8,14 +9,14 @@ const assertArraysEqual = function (array1, array2) {
 };
 
 
-const eqArrays = function (array1, array2) {
-  let isEqual = true;
-  for (let j = 0; j < array1.length; j++) {
-    if (array1[j] !== array2[j]) {
-      return isEqual = false;
-    }
-  }
-  return isEqual;
-};
+// const eqArrays = function (array1, array2) {
+//   let isEqual = true;
+//   for (let j = 0; j < array1.length; j++) {
+//     if (array1[j] !== array2[j]) {
+//       return isEqual = false;
+//     }
+//   }
+//   return isEqual;
+// };
 // assertArraysEqual([1, 2, 3], [1, 2, 3]);
 module.exports = assertArraysEqual;
