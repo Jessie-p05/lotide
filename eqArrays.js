642053f8f65cf2f564174b609fb6f19d60082bbc
Jessie-p05 <jxpei05@gmail.com>
2021-04-05 20:46:30 +0000

    rafactor the eqArrays.js with seperated assertion file

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,17 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-// assertEqual();
-
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   let isEqual = true;
   for (let j = 0; j < array1.length; j++) {
     if (array1[j] !== array2[j]) {
       return isEqual = false;
     }
-      
+
   }
-    
+
   return isEqual;
 };
 
 
+module.exports = eqArrays;
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
