c8e79dc7abeb8e2b7bdb42f402b2d770b129e35a
Jessie-p05 <jxpei05@gmail.com>
2021-04-06 02:31:44 +0000

    refactor the middleTest.js and tailTest.js withmocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,27 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArrayEqual = require('../assertArraysEqual');
 
 
-assertArrayEqual(middle([1, 2, 3]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArrayEqual(middle([1, 2, 3]), [2]);
+// assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+
+
+
+describe("#middle", () => {
+  it('should returns [2] when pass [1,2,3]', () => {
+    const result = middle([1, 2, 3]);
+    assert.notStrictEqual(result, [2]);
+  })
+  it('should returns [2,3] when pass [1,2,3,4]', () => {
+    const result = middle([1, 2, 3, 4]);
+    assert.notStrictEqual(result, [2, 3]);
+  })
+  it('should returns [3,4] when pass [1,2,3,4,5,6]', () => {
+    const result = middle([1, 2, 3, 4, 5, 6]);
+    assert.notStrictEqual(result, [3, 4]);
+  })
+
+
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require("../tail");
 
+describe("#tail", () => {
+  it('should returns ["Lighthouse", "Labs"] when pass ["Hello", "Lighthouse", "Labs"]', () => {
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]);
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.notStrictEqual(result, ["Lighthouse", "Labs"]);
+
+  })
+})
